69531ed4101738b4410116f832ca2c7e4b0cfc2d
Isabel Muniz <isabelandreamuniz@gmail.com>
2023-10-31 15:13:40 -0400

    Added dark mode.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import ToDoList from "./components/ToDoList";
 
 function App() {
   const currentYear = new Date().getFullYear();
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
 
   return (
-    <div>
+    <div className={`App ${isDarkMode ? "dark-mode" : ""}`}>
+      <div className="form-check form-switch">
+        <input
+          onClick={toggleDarkMode}
+          className="form-check-input"
+          type="checkbox"
+          role="switch"
+          id="flexSwitchCheckDefault"
+        />
+        <label className="form-check-label">Dark Mode</label>
+      </div>
       <ToDoList></ToDoList>
       <footer className="footer">
         <strong>
